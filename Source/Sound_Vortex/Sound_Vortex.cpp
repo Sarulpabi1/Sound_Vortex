754d2cdcde959bf4ff738813bc980ebcfb135441
Keuvain <118433891+Sarulpabi1@users.noreply.github.com>
2024-02-28 14:23:47 +0100

    Main

---
+++ Source/Sound_Vortex/Sound_Vortex.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sound_Vortex.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sound_Vortex, "Sound_Vortex" );
